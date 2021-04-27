8d5ff91d34a66fde3f6a1dad8d1a45a1af3f1bdd
JCassio1 <Joselsondias@hotmail.com>
2021-04-27 14:41:04 +0100

    Change filename and add express server routes

---
+++ calculator.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+const app = express();
+
+//Server routes
+app.get("/", function(req, res){
+  res.sendFile(__dirname + "/index.html"); //__dirname. to get full path
+});
+
+app.get("/contact", function(req, res){
+  res.send("Email me at joselsondias@hotmail.com");
+});
+
+
+//Listening
+app.listen(3000, function(){
+  console.log("Server started in port 3000");
+});
